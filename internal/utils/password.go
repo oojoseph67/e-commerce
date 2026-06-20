7580022a26c9c9d4365c94bd618cb7e19a8a770f
Joseph <oojoseph67@gmail.com>
2026-06-20 22:38:12 +0100

    chore: password utils added

---
+++ internal/utils/password.go
@@ -0,0 +1,25 @@
+package utils
+
+import "golang.org/x/crypto/bcrypt"
+
+// Hashes user password so we can save it to the database
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+
+	if err != nil {
+		return "", err
+	}
+
+	return string(bytes), nil
+}
+
+// Unhash and compare password to check user authenticity
+func ComparePassword(password, hashedPassword string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+
+	if err != nil {
+		return false
+	}
+
+	return true
+}
