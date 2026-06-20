ca421d0cfda1cb185d01a89b8849bd3a5a7e0b39
Joseph <oojoseph67@gmail.com>
2026-06-20 22:25:33 +0100

    feat: jwt util added

--- internal/dto/auth.dto.go
+++ internal/dto/auth.dto.go
@@ -1,5 +1,7 @@
 package dto
 
+// binding is gotten from go-gin
+
 type RegisterRequest struct {
 	Email     string `json:"email" binding:"required,email"`
 	Password  string `json:"password" binding:"required,min-8"`
---
+++ internal/utils/jwt.go
@@ -0,0 +1,75 @@
+package utils
+
+import (
+	"errors"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/oojoseph67/ecommerce/internal/config"
+)
+
+type Claims struct {
+	UserID uint   `json:"user_id"`
+	Email  string `json:"email"`
+	Role   string `json:"role"`
+	jwt.RegisteredClaims
+}
+
+// Generates access and refresh tokens
+func GenerateTokenPair(config *config.JWTConfig, userId uint, email, role string) (accessToken, refreshToken string, err error) {
+	// creating accessToken
+	accessClaims := &Claims{
+		UserID: userId,
+		Email:  email,
+		Role:   role,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(config.ExpiresIn)),
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+		},
+	}
+
+	at := jwt.NewWithClaims(jwt.SigningMethodES256, accessClaims)
+	accessTokenString, err := at.SignedString([]byte(config.Secret))
+
+	if err != nil {
+		return "", "", err
+	}
+
+	// creating refreshToken
+	refreshClaims := &Claims{
+		UserID: userId,
+		Email:  email,
+		Role:   role,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(config.RefreshTokenExpires)),
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+		},
+	}
+
+	rt := jwt.NewWithClaims(jwt.SigningMethodES256, refreshClaims)
+	refreshTokenString, err := rt.SignedString([]byte(config.Secret))
+
+	if err != nil {
+		return "", "", err
+	}
+
+	return accessTokenString, refreshTokenString, nil
+}
+
+// Checks if accessToken or refreshToken is valid
+func ValidateToken(tokenString, secret string) (claims *Claims, isValid bool, err error) {
+
+	token, err := jwt.ParseWithClaims(tokenString, &Claims{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte(secret), nil
+	})
+
+	if err != nil {
+		return nil, false, err
+	}
+
+	if claims, ok := token.Claims.(*Claims); ok && token.Valid {
+		return claims, true, nil
+	}
+
+	return nil, false, errors.New("invalid token")
+}
